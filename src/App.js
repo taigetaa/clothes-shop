555c108249c9f6f0de3f0de0977dd7c9822adff7
Victoria <bethmorgendorffer@mail.ru>
2023-12-26 15:53:10 +0300

    routing and fixes for deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 
