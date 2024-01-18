1607faf9e3ba9a3c686aacd092552f6222c9bc3b
Victoria <bethmorgendorffer@mail.ru>
2024-01-18 16:59:53 +0300

    cart count

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,15 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
 import './product-card.styles.scss';
 
 import Button from '../button/button.component';
 
 const ProductCard = ({ product }) => {
+  const { addItemToCart } = useContext(CartContext);
   const { name, price, imageUrl } = product;
+
+  const addProductToToAdd = () => addItemToCart(product);
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
@@ -11,7 +17,14 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to card</Button>
+      <Button
+        buttonType="inverted"
+        onClick={() => {
+          addProductToToAdd(product);
+        }}
+      >
+        Add to card
+      </Button>
     </div>
   );
 };
